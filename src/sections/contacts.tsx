71440490ac57ef46ef038bad8b044734a3d50a97
Dmitriy Aleksandrovich Chernukho <gormonn@redcollar.ru>
2023-08-23 02:18:56 +0300

    add contacts form

--- src/sections/contacts.tsx
+++ src/sections/contacts.tsx
@@ -8,5 +8,18 @@ export const Contacts:FC<ComponentPropsWithoutRef<'section'>> = (props) => {
             <h3>Contact Us</h3>
             <h1>Contact us to find out more</h1>
         </Row>
+        <Row>
+            <form className={css.section__form}>
+                <label>
+                    Your Name
+                    <input/>
+                </label>
+                <label>
+                    Your Phone
+                    <input/>
+                </label>
+                <button type='submit' className={css.section__form_submit}>Request a Call Back</button>
+            </form>
+        </Row>
     </section>
 }
